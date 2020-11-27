615066ba9207f2265b084493ddee1d964a348e1f
Grzegorz <serwacy2@gmail.com>
2020-11-27 17:12:21 +0100

    initial commit

---
+++ src/main/groovy/com/gruzini/gramophone/GramophoneApplication.groovy
@@ -0,0 +1,13 @@
+package com.gruzini.gramophone
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class GramophoneApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(GramophoneApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/gruzini/gramophone/GramophoneApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.gruzini.gramophone
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GramophoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
